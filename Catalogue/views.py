eb72517db37c016c24d44eb1cdc6dabe7dc08779
Olivia Bizimungu <oibizimungu@mta.ca>
2021-02-23 15:44:33 -0400

    fake change

--- Catalogue/views.py
+++ Catalogue/views.py
@@ -4,10 +4,13 @@ from .models import Fossil
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Catalog Home Page.</h1> \n <p>This could handle login in and navigation to other parts of the website</p>")
+    return HttpResponse(
+        "<h1>Catalog Home Page.</h1> \n <p>This could handle login in and navigation to other parts of the website</p>"
+    )
+
 
 def items(request):
-    theEntries = {
-        'entries': Fossil.objects.all()
-    }
-    return render(request, 'catalogue/items.html', theEntries)
+    theEntries = {"entries": Fossil.objects.all()}
+    return render(request, "catalogue/items.html", theEntries)
+
+    # fake change
