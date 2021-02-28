8bc5c27e7b98a47c273b88a1b1969aef28b1f926
Olivia Bizimungu <oibizimungu@mta.ca>
2021-02-28 14:10:49 -0400

    View started

--- Catalogue/urls.py
+++ Catalogue/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-#Import the views of the Catalogue application so they can be routed to
-from . import views 
+from django.conf.urls import url
 
+# Import the views of the Catalogue application so they can be routed to
+from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="Catalogue-Index"), #By default route to the home page (index)
-    path("items/", views.items, name="Items") #route to page showing list of items in musuem
+    path("", views.browse),
+    path("browse/", views.browse),
+    path("login/", views.login),
+    path("search/", views.search),
 ]
--- Catalogue/views.py
+++ Catalogue/views.py
@@ -1,14 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Fossil
+from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
-def index(request):
-    return HttpResponse(
-        "<h1>Catalog Home Page.</h1> \n <p>This could handle login in and navigation to other parts of the website</p>"
-    )
 
 
-def items(request):
-    theEntries = {"entries": Fossil.objects.all()}
-    return render(request, "catalogue/items.html", theEntries)
+def login(request):
+    return render(request, "login.html")
+
+
+def search(request):
+    return render(request, "search.html")
+
+
+def browse(request):
+    context = {
+        "records": [
+            {
+                "user": "Jared",
+                "catalogue_number": "",
+                "name": "Canon AE-1",
+                "type": "camera",
+                "date": "1990",
+                "manufacturer": "Canon",
+                "condition": "3",
+                "provenance": "Inherited",
+                "description": "This is my favourite piece in my collection",
+                "additional_info": [("battery", "3V")],
+                "has_image": True,
+            },
+            {
+                "user": "Susan",
+                "catalogue_number": "",
+                "name": "Hasselblad 700",
+                "type": "camera",
+                "date": "1978",
+                "manufacturer": "Hassbelblad",
+                "condition": "4.5",
+                "provenance": "Purchased new",
+                "description": "Best camera",
+                "additional_info": [("lens", "50mm")],
+                "has_image": True,
+            },
+            {
+                "user": "Will",
+                "catalogue_number": "",
+                "name": "Minolta X-700",
+                "type": "camera",
+                "date": "2001",
+                "manufacturer": "Minolta",
+                "condition": "2",
+                "provenance": "Bought second hand",
+                "description": "Decent camera.",
+                "additional_info": [],
+                "has_image": False,
+            },
+        ]
+    }
+    return render(request, "browse.html", context)
