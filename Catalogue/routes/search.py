8dfe14699af84e182e0448cd15ca81d397f351bf
Eamon <eamccarron@mta.ca>
2021-03-01 15:59:14 -0400

    Removed semicolons

--- Catalogue/routes/search.py
+++ Catalogue/routes/search.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def search_route(request):
-    return render(request, "search.html");
+    return render(request, "search.html")
