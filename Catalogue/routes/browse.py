e68a8096b86eff8e3b40aab3785eda39909fb56b
Olivia Bizimungu <oibizimungu@mta.ca>
2021-03-01 21:32:46 -0400

    initial commit

--- Catalogue/routes/browse.py
+++
@@ -1,8 +0,0 @@
-
-from django.http import HttpResponse
-from django.shortcuts import render
-from Catalogue.models import Fossil, Record
-
-def browse_route(request):
-    context = {"records": Record.objects.all()}
-    return render(request, "browse.html", context)
