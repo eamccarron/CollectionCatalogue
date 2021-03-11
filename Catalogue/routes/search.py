b849d657aebadae1634b83ff6c1e0f436255ad43
Olivia Bizimungu <oibizimungu@mta.ca>
2021-03-10 23:51:29 -0400

    Implemented search & filter by item type

--- Catalogue/routes/search.py
+++ Catalogue/routes/search.py
@@ -1,6 +1,25 @@
-
 from django.http import HttpResponse
 from django.shortcuts import render
+from Catalogue.models import Record
+from django.db.models import Q
+
 
 def search_route(request):
-    return render(request, "search.html");
+    if request.method == "GET":
+        records = Record.objects.all()
+
+        if "catalogue" in request.GET:
+            catalogue = request.GET.__getitem__("catalogue")
+            if catalogue:
+                records = records.filter(item_type__icontains=catalogue).all()
+
+        if "val" in request.GET:
+            search_val = request.GET.__getitem__("val")
+            if search_val:
+                records = records.filter(
+                    Q(name__icontains=search_val) | Q(description__icontains=search_val)
+                )
+
+        context = {"records": records}
+
+        return render(request, "search.html", context)
