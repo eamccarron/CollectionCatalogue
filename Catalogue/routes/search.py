a5a07e5109681d6f3f2a3adee25a1eb4ae72aa66
Olivia Bizimungu <oibizimungu@mta.ca>
2021-03-13 16:52:45 -0400

    Extended search and custom filters

--- Catalogue/routes/search.py
+++ Catalogue/routes/search.py
@@ -20,6 +20,11 @@ def search_route(request):
                     Q(name__icontains=search_val) | Q(description__icontains=search_val)
                 )
 
+        for key, val in request.GET.items():
+            if key != "catalogue" and key != "val":
+                filt_param = Q((f"{key}__contains", f"{val}"))
+                records = records.filter(filt_param)
+
         context = {"records": records}
 
         return render(request, "search.html", context)
