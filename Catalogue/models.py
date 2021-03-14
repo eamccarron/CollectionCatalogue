43456221feffdf96f9f88ee42ed91a4f6a3aff4c
msgallant <71039715+msgallant@users.noreply.github.com>
2021-03-13 20:02:03 -0400

    more model templates

--- Catalogue/models.py
+++ Catalogue/models.py
@@ -5,25 +5,120 @@ from django.contrib.auth.models import User
 class Record(models.Model):
     catalogue_num = models.IntegerField(default=1, primary_key=True) #default is what is registered as value if no other value is received
     name = models.CharField(max_length=100) #string that has a max length of 100 characters
-    date = models.CharField(max_length=100, default='na') #up to 100 characters, so it can give a description or range of dates
-    #manufacturer = models.CharField(max_length=100)
-    condition = models.TextField(default='na') #unrestricted text
-    provenance = models.CharField(max_length=100, default='na') #the origin of item
-    description = models.TextField(default='na') #unrestricted text
-    item_type = models.CharField(max_length=100, default='na')
+    date = models.CharField(max_length=100, blank=True, null=True) #up to 100 characters, so it can give a description or range of dates
+    condition = models.TextField(blank=True, null=True) #unrestricted text
+    provenance = models.TextField(blank=True, null=True) #the origin of item
+    description = models.TextField(blank=True, null=True) #unrestricted text
+    value = models.IntegerField(blank=True, null=True)
+    item_type = models.CharField(max_length=100, blank=True, null=True)
     staff_creator = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL) #one to many relationship, a user can create many base models but 
     #a base model may only be created by 1 user. cascade on delete so the entry doesn't still exist in the users table.
-    #add dictionary --> for other attributes
+    
     def __str__(self):
         return self.name
 
 class Fossil(models.Model):
     record = models.OneToOneField(Record, on_delete=models.CASCADE)
     size = models.IntegerField(blank=True, null=True) #allow field to be left blank and set to null 
-    scientific_name = models.CharField(max_length=100, default='na') 
+    scientific_name = models.CharField(max_length=100, blank=True, null=True) 
+
 
     def __str__(self):
         return self.record.name
+
+class Sculpture(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    material = models.CharField(max_length=100, blank=True, null=True) #material used to build sculpture
+    artist = models.CharField(max_length=100, blank=True, null=True)
+    colour = models.CharField(max_length=100, blank=True, null=True)
+
+
+class Coin(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    colour = models.CharField(max_length=100, blank=True, null=True)
+    material = models.CharField(max_length=100, blank=True, null=True)
+    coin_value = models.CharField(max_length=100, blank=True, null=True)
+    design = models.TextField(blank=True, null=True) # what object or person (user able to put extra information about the person) is on the coin if any
+
+class Jewelry(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    JEWELRY_TYPES = (
+        ('Pendant', 'Pendant'),
+        ('Bracelet', 'Bracelet'),
+        ('Cufflink', 'Cufflink'),
+        ('Earrings','Earrings'),
+        ('Necklace','Necklace'),
+        ('Ring','Ring'),
+        ('Brooche', 'Brooche'),
+        ('Gem', 'Gem'),
+        ('Other','Other')
+    )
+    jewelry_type = models.CharField(max_length=10, choices=JEWELRY_TYPES)
+    colour = models.CharField(max_length=100, blank=True, null=True)
+    material = models.CharField(max_length=100, blank=True, null=True)
+    size = models.IntegerField(blank=True, null=True)
+    weight = models.IntegerField(blank=True, null=True)
+
+class Meteorite(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    size = models.IntegerField(blank=True, null=True)
+    weight = models.IntegerField(blank=True, null=True)
+    colour = models.CharField(max_length=100, blank=True, null=True)
+    material = models.CharField(max_length=100, blank=True, null=True)
+
+class Artwork(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    artist = models.CharField(max_length=100, blank=True, null=True)
+    artist_lifespan = models.TextField(blank=True, null=True)
+    dimensions = models.CharField(max_length=100, blank=True, null=True)
+    supplies_used = models.CharField(max_length=100, blank=True, null=True) #paintbrush, pencil, etc
+    art_style = models.TextField(blank=True, null=True)
+
+class Pottery(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    artist = models.CharField(max_length=100, blank=True, null=True)
+    artist_lifespan = models.TextField(blank=True, null=True)
+    material = models.CharField(max_length=100, blank=True, null=True)
+    technique = models.TextField(blank=True, null=True)
+
+class Medal(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    medal_type = models.CharField(max_length=100, blank=True, null=True) #what was this medal awarded for
+    colour = models.CharField(max_length=100, blank=True, null=True)
+    material = models.CharField(max_length=100, blank=True, null=True)
+    design = models.TextField(blank=True, null=True) #what design is on the medal
+    recipient = models.TextField(blank=True, null=True)
+
+class Weapon(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    manufacturer = models.CharField(max_length=100, blank=True, null=True)
+    weapon_type = models.CharField(max_length=100, blank=True, null=True)
+    combat = models.CharField(max_length=100, blank=True, null=True) #is this weapon long distance or close combat
+    size = models.IntegerField(blank=True, null=True)
+    weight = models.IntegerField(blank=True, null=True)
+    material = models.CharField(max_length=100, blank=True, null=True)
+
+class Book(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    author = models.TextField(blank=True, null=True) #it is text field in case there are many authors
+    illustrator = models.TextField(blank=True, null=True)
+    language = models.CharField(max_length=100, blank=True, null=True)
+    genre = models.CharField(max_length=100, blank=True, null=True)
+    publisher = models.CharField(max_length=100, blank=True, null=True)
+
+class Photo(models.Model):
+    record = models.OneToOneField(Record, on_delete=models.CASCADE)
+    photographer = models.CharField(max_length=100, blank=True, null=True)
+
+
+
+
+
+
+
+
+
+    
 
 
 
