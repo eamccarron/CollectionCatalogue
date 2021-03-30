163b5db3b09cee5d075a8b44650dbafc90c30034
Eamon <eamccarron@mta.ca>
2021-03-30 11:17:34 -0300

    Routed login to Django admin site

--- Catalogue/urls.py
+++ Catalogue/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.conf.urls import include
 
 # Import the views of the Catalogue application so they can be routed to
 from . import views
@@ -11,6 +12,5 @@ urlpatterns = [
     path("browse/", views.browse, name="browse"),
     path("login/", views.login, name="login"),
     path("search/", views.search, name="search"),
-    path("logout/", views.logout, name="logout"),
-    
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
