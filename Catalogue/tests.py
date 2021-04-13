905d186bc701210aa6c4264f443771082f9c718e
Eamon <eamccarron@mta.ca>
2021-04-13 12:31:46 -0300

    Aded unit test for record creation

--- Catalogue/tests.py
+++ Catalogue/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
 
-# Create your tests here.
+from Catalogue.models import *
+
+Test_Record = {
+    "catalogue_num" : 1,
+    "name" : "Test Record",
+    "date" : "Sept. 27, 1998",
+    "condition" : "Good",
+    "provenance" : "Test Provenance",
+    "description" : "Test Description",
+    "item_type" : "Record"
+}
+
+class BaseRecord(TestCase):
+    def setUp(self):
+        Record.objects.create(
+            catalogue_num = Test_Record["catalogue_num"],
+            name = Test_Record["name"], 
+            date = Test_Record["date"],
+            condition = Test_Record["condition"],
+            provenance = Test_Record["provenance"],
+            description = Test_Record["description"]
+        )
+    
+    def test_record_can_be_created():
+        self.assertNotEqual(Record.objects.get(Test_Record["catalogue_num"]), None)
+
+    def test_all_record_fields_can_be_read():
+        #Store record created in setUp
+        saved_record = Record.objects.get(Test_Record.catalogue_num)
+        #Read each field of record
+        catalogue_num_field = saved_record._meta.get_field('catalogue_num') 
+        name_field = saved_record._meta.get_field('name') 
+        date_field = saved_record._meta.get_field('date') 
+        condition_field = saved_record._meta.get_field('condition') 
+        provenance_field = saved_record._meta.get_field('provenance') 
+
+        #Test that each stored field is equal to the value stored in testRecord
+        self.assertEqual(catalogue_num_field.value_from_object(saved_record), Test_Record.catalogue_num)
+        self.assertEqual(name_field.value_from_object(saved_record), Test_Record.catalogue_num)
+        self.assertEqual(date_field.value_from_object(saved_record), Test_Record.catalogue_num)
+        self.assertEqual(condition_field.value_from_object(saved_record), Test_Record.catalogue_num)
+        self.assertEqual(provenance_field.value_from_object(saved_record), Test_Record.catalogue_num)
+
